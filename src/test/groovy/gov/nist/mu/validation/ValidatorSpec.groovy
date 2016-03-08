f069b52d669d0251461dbfdb29283464cc8e2980
Rahul Somasunderam <rahul.som@gmail.com>
2016-03-07 21:05:29 -0800

    Add support for BPPC

--- src/test/groovy/gov/nist/mu/validation/ValidatorSpec.groovy
+++ src/test/groovy/gov/nist/mu/validation/ValidatorSpec.groovy
@@ -5,16 +5,16 @@ import spock.lang.Specification
 class ValidatorSpec extends Specification {
     def "sample ccd should validate"() {
         when: "I validate a CCD"
-        def nodes = Validator.validate(
+        def results = Validator.validate(
                 file('SampleCCDDocument.xml'),
                 Rulesets.Ccd, Rulesets.Cda4Cdt, Rulesets.C32_V25_C83_20
         )
 
-        then: "There are 3 nodes"
-        nodes.size() == 3
+        then: "There are 3 results"
+        results.size() == 3
 
         and: "Some other assertion"
-        nodes*.issues.flatten().findAll().size() == 54
+        results*.issues.flatten().findAll().size() == 54
     }
 
     private InputStream file(String resourceName) {
@@ -23,57 +23,71 @@ class ValidatorSpec extends Specification {
 
     def "MU Rev0 has 4 errors"() {
         when: "I validate a CCD"
-        def nodes = Validator.validate(
+        def results = Validator.validate(
                 file('MUExamplesJan2011/MU_Rev0_HITSP_BaseC32v2.5_RequiredTemplateIds_FourErrors.xml'),
                 Rulesets.Ccd, Rulesets.Cda4Cdt, Rulesets.C32_V25_C83_20
         )
 
-        then: "There are 3 nodes"
-        nodes.size() == 3
+        then: "There are 3 results"
+        results.size() == 3
 
         and: "Some other assertion"
-        nodes*.issues.flatten().findAll().size() == 4
+        results*.issues.flatten().findAll().size() == 4
     }
 
     def "MU Rev1 has no errors"() {
         when: "I validate a CCD"
-        def nodes = Validator.validate(
+        def results = Validator.validate(
                 file('MUExamplesJan2011/MU_Rev1_HITSP_C32C83_4Sections_NoInformationEntries_NoErrors.xml'),
                 Rulesets.Ccd, Rulesets.Cda4Cdt, Rulesets.C32_V25_C83_20
         )
 
-        then: "There are 3 nodes"
-        nodes.size() == 3
+        then: "There are 3 results"
+        results.size() == 3
 
         and: "Some other assertion"
-        nodes*.issues.flatten().findAll().size() == 0
+        results*.issues.flatten().findAll().size() == 0
     }
 
     def "MU Rev2 has no errors"() {
         when: "I validate a CCD"
-        def nodes = Validator.validate(
+        def results = Validator.validate(
                 file('MUExamplesJan2011/MU_Rev2_HITSP_C32C83_4Sections_MeaningfulEntryContent_NoErrors.xml'),
                 Rulesets.Ccd, Rulesets.Cda4Cdt, Rulesets.C32_V25_C83_20
         )
 
-        then: "There are 3 nodes"
-        nodes.size() == 3
+        then: "There are 3 results"
+        results.size() == 3
 
         and: "Some other assertion"
-        nodes*.issues.flatten().findAll().size() == 0
+        results*.issues.flatten().findAll().size() == 0
     }
 
     def "MU Rev3 has no errors"() {
         when: "I validate a CCD"
-        def nodes = Validator.validate(
+        def results = Validator.validate(
                 file('MUExamplesJan2011/MU_Rev3_HITSP_C32C83_4Sections_RobustEntries_NoErrors.xml'),
                 Rulesets.Ccd, Rulesets.Cda4Cdt, Rulesets.C32_V25_C83_20
         )
 
-        then: "There are 3 nodes"
-        nodes.size() == 3
+        then: "There are 3 results"
+        results.size() == 3
 
         and: "Some other assertion"
-        nodes*.issues.flatten().findAll().size() == 0
+        results*.issues.flatten().findAll().size() == 0
+    }
+
+    def "BPPC has no errors"() {
+        when: "I validate a CCD"
+        def results = Validator.validate(
+                file('IHE_BPPC_2010Dec10/BPPCExampleFromIHE.xml'),
+                Rulesets.IheBppc
+        )
+
+        then: "There are 3 results"
+        results.size() == 1
+
+        and: "Some other assertion"
+        results*.issues.flatten().findAll().size() == 2
     }
 }
