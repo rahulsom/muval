523e8f3944f4d10b2c3e57639e3ef893d4f7ed8e
Rahul Somasunderam <rahul.som@gmail.com>
2016-03-11 17:38:18 -0800

    Improve documentation

--- src/test/groovy/gov/nist/mu/validation/ValidatorSpec.groovy
+++ src/test/groovy/gov/nist/mu/validation/ValidatorSpec.groovy
@@ -2,18 +2,18 @@ package gov.nist.mu.validation
 
 import spock.lang.Specification
 
+import static gov.nist.mu.validation.Rulesets.*
+import static gov.nist.mu.validation.Validator.validate
+
 class ValidatorSpec extends Specification {
-    
+
     private InputStream file(String resourceName) {
         this.class.getClassLoader().getResourceAsStream(resourceName)
     }
 
     def "sample ccd should validate"() {
         when: "I validate a CCD"
-        def result = Validator.validate(Rulesets.Cdar2c32,
-                file('SampleCCDDocument.xml'),
-                Rulesets.Ccd, Rulesets.Cda4Cdt, Rulesets.C32_v_2_5_c83_2_0
-        )
+        def result = validate(Cdar2c32, file('SampleCCDDocument.xml'), Ccd, Cda4Cdt, C32_v_2_5_c83_2_0)
 
         then: "There are 50 errors and 98 issues"
         result.allIssues.size() == 98
@@ -23,9 +23,9 @@ class ValidatorSpec extends Specification {
 
     def "MU Rev0 has 0 errors"() {
         when: "I validate a CCD"
-        def result = Validator.validate(Rulesets.Cdar2c32,
+        def result = validate(Cdar2c32,
                 file('MUExamplesJan2011/MU_Rev0_HITSP_BaseC32v2.5_RequiredTemplateIds_FourErrors.xml'),
-                Rulesets.Ccd, Rulesets.Cda4Cdt, Rulesets.C32_v_2_5_c83_2_0
+                Ccd, Cda4Cdt, C32_v_2_5_c83_2_0
         )
 
         then: "There are 0 errors and 4 issues "
@@ -36,9 +36,9 @@ class ValidatorSpec extends Specification {
 
     def "MU Rev1 has no errors"() {
         when: "I validate a CCD"
-        def result = Validator.validate(Rulesets.Cdar2c32,
+        def result = validate(Cdar2c32,
                 file('MUExamplesJan2011/MU_Rev1_HITSP_C32C83_4Sections_NoInformationEntries_NoErrors.xml'),
-                Rulesets.Ccd, Rulesets.Cda4Cdt, Rulesets.C32_v_2_5_c83_2_0
+                Ccd, Cda4Cdt, C32_v_2_5_c83_2_0
         )
 
         then: "There are 0 errors and 32 issues "
@@ -49,9 +49,9 @@ class ValidatorSpec extends Specification {
 
     def "MU Rev2 has no errors"() {
         when: "I validate a CCD"
-        def result = Validator.validate(Rulesets.Cdar2c32,
+        def result = validate(Cdar2c32,
                 file('MUExamplesJan2011/MU_Rev2_HITSP_C32C83_4Sections_MeaningfulEntryContent_NoErrors.xml'),
-                Rulesets.Ccd, Rulesets.Cda4Cdt, Rulesets.C32_v_2_5_c83_2_0
+                Ccd, Cda4Cdt, C32_v_2_5_c83_2_0
         )
 
         then: "There are 0 errors and 53 issues "
@@ -62,9 +62,9 @@ class ValidatorSpec extends Specification {
 
     def "MU Rev3 has no errors"() {
         when: "I validate a CCD"
-        def result = Validator.validate(Rulesets.Cdar2c32,
+        def result = validate(Cdar2c32,
                 file('MUExamplesJan2011/MU_Rev3_HITSP_C32C83_4Sections_RobustEntries_NoErrors.xml'),
-                Rulesets.Ccd, Rulesets.Cda4Cdt, Rulesets.C32_v_2_5_c83_2_0
+                Ccd, Cda4Cdt, C32_v_2_5_c83_2_0
         )
 
         then: "There are 0 errors and 44 issues "
@@ -75,10 +75,7 @@ class ValidatorSpec extends Specification {
 
     def "BPPC has no errors"() {
         when: "I validate a CCD"
-        def result = Validator.validate(Rulesets.Cdar2c32,
-                file('IHE_BPPC_2010Dec10/BPPCExampleFromIHE.xml'),
-                Rulesets.IheBppc
-        )
+        def result = validate(Cdar2c32, file('IHE_BPPC_2010Dec10/BPPCExampleFromIHE.xml'), IheBppc)
 
         then: "There are 0 errors and 2 issues "
         result.allIssues.size() == 2
